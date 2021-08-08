9b271e4202ea535a1717b883915d408638db06a3
hyobumjung <hyobumjung@houstep.co.kr>
2021-08-08 09:27:19 +0900

    Feat: constructor 가 필요한 테스트 테스트 완료

--- src/filters/global-exception.filter.spec.ts
+++ src/filters/global-exception.filter.spec.ts
@@ -1,7 +1,27 @@
-// import { GlobalExceptionFilter } from './global-exception.filter';
+import { GlobalExceptionFilter } from './global-exception.filter';
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
+import { ConfigService } from '@nestjs/config';
+import { PROVIDE } from 'src/references';
 
 describe('GlobalExceptionFilter', () => {
+
+  let app: INestApplication;
+  let config: ConfigService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    })
+      .compile();
+    
+    app = module.createNestApplication();
+    await app.init();
+    config = app.get(ConfigService);
+  })
+
   it('should be defined', () => {
-    // expect(new GlobalExceptionFilter()).toBeDefined();
+    expect(new GlobalExceptionFilter(app.get(PROVIDE.LOGGER), config, app.get(PROVIDE.SLACK))).toBeDefined();
   });
 });
